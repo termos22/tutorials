bf3261557cfa601510fa38fd9b4e80b7c41dbb0c
Jakub Przybysz <termos22@gmail.com>
2024-03-07 13:50:27 +0100

    trying to fix falling line, add few processing examples

---
+++ falling_line_from_processing.py
@@ -0,0 +1,23 @@
+import math
+from processing_py import *
+
+
+app = App(900, 900)
+
+def draw():
+  angle = 90
+  app.background(0)
+  app.translate(app.width/2, app.height/2)
+  app.stroke(255)
+  app.strokeWeight(3)
+  for i in range(0, 280, 10):
+    app.pushMatrix()
+    app.rotate(app.radians(i) * app.cos(app.radians(angle)))
+    app.line(200 * math.sin(math.radians(angle)), 0, 0, 200)
+    app.popMatrix()
+    app.redraw()
+  angle += 1
+
+
+while True:
+  draw()
