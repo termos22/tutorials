2359b567da0891095543a8b3c898158e49aec276
Jakub Przybysz <termos22@gmail.com>
2024-03-12 18:14:21 +0100

    changed functions to use Processing, only variable angle is not working properly

--- falling_line2_from_processing.py
+++ falling_line2_from_processing.py
@@ -3,26 +3,28 @@
 # wygląda na to, że wymaga modułu/biblioteki processing-py.
 
 import math
+from processing_py import *
+
 angle = 90 # Deklaracja zmiennej
  
 def setup(): # Funkcja setup
-  size(900, 900) # Ustaw rozmiar okna
+  App(900, 900) # Ustaw rozmiar okna
   
 def draw(): # Funkcja draw
-  background(0) # Wyczyść ekran
-  translate(width/2, height/2) # Przesuń układ współrzędnych do środka okna
-  stroke(255) # Ustaw kolor i grubość linii
-  strokeWeight(3) # Pętla rysująca linie
+  App.background(0) # Wyczyść ekran
+  App.translate(App.width/2, App.height/2) # Przesuń układ współrzędnych do środka okna
+  App.stroke(255) # Ustaw kolor i grubość linii
+  App.strokeWeight(3) # Pętla rysująca linie
   for i in range(0, 280, 10):
-    pushMatrix() # Zapisz stan transformacji
-    rotate(math.radians(i) * math.cos(math.radians(angle))) # Obróć układ współrzędnych
-    line(200 * math.sin(math.radians(angle)), 0, 0, 200) # Narysuj linię
-    popMatrix() # Przywróć stan transformacji
+    App.pushMatrix() # Zapisz stan transformacji
+    App.rotate(math.radians(i) * math.cos(math.radians(angle))) # Obróć układ współrzędnych
+    App.line(200 * math.sin(math.radians(angle)), 0, 0, 200) # Narysuj linię
+    App.popMatrix() # Przywróć stan transformacji
   angle += 1 # Zwiększ kąt
  
 setup() # Wywołaj funkcję setup
  
-while True: # Uruchamij pętlę draw
+while True: # Uruchamia pętlę draw
   draw()
   
   
