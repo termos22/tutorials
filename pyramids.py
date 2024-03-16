02c14f089af5ec8f4aae166cc7097c197de8584e
Jakub Przybysz <termos22@gmail.com>
2024-03-16 06:58:18 +0100

    fixed capital letter issue in code pyramids.py

--- pyramids.py
+++ pyramids.py
@@ -1,27 +1,27 @@
 
 # 1. Pyramid
-print("\nNormal Pyramid)
+print("Normal Pyramid")
 for i in range(5):
     x="*"
     x=x*i
     print(f"{x:^10}")
 
 # 2. Invert Pyramid
-Print("\nInvert Pyramid")
+print("Invert Pyramid")
 for i in range(5):
     x="*"
     x=x*(5-i)
     print(f"{x:^10}")
 
 # 3. Left sided Pyramid
-Print("\nLeft Sided Pyramid")
+print("Left Sided Pyramid")
 for i in range(5):
     x="*"
     x=x*i
     print(f"{x:<10}")
 
 # 4. Right sided Pyramid
-Print("\nRight Sided Pyramid")
+print("Right Sided Pyramid")
 for i in range(5):
     x="*"
     x=x*i
