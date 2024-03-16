4e1b7c519ed57a0319ba8e5cf24090abe93205f0
Jakub Przybysz <termos22@gmail.com>
2024-03-16 06:39:38 +0100

    working with falling line to work

--- falling_line2_from_processing.py
+++ falling_line2_from_processing.py
@@ -5,28 +5,31 @@
 import math
 from processing_py import *
 
-angle = 90 # Deklaracja zmiennej
+#angle = 90 # Deklaracja zmiennej
  
-def setup(): # Funkcja setup
-  App(900, 900) # Ustaw rozmiar okna
-  
+#def setup(): # Funkcja setup
+#  app=App(900,900) # Ustaw rozmiar okna
+app=App(900,900) # Ustaw rozmiar okna
+ 
 def draw(): # Funkcja draw
-  App.background(0) # Wyczyść ekran
-  App.translate(App.width/2, App.height/2) # Przesuń układ współrzędnych do środka okna
-  App.stroke(255) # Ustaw kolor i grubość linii
-  App.strokeWeight(3) # Pętla rysująca linie
+  angle = 90
+  app.background(0,0,0) # Wyczyść ekran
+  app.translate(app.width/2, app.height/2) # Przesuń układ współrzędnych do środka okna
+  app.stroke(255) # Ustaw kolor i grubość linii
+  app.strokeWeight(3) # Pętla rysująca linie
   for i in range(0, 280, 10):
-    App.pushMatrix() # Zapisz stan transformacji
-    App.rotate(math.radians(i) * math.cos(math.radians(angle))) # Obróć układ współrzędnych
-    App.line(200 * math.sin(math.radians(angle)), 0, 0, 200) # Narysuj linię
-    App.popMatrix() # Przywróć stan transformacji
-  angle += 1 # Zwiększ kąt
+    app.pushMatrix() # Zapisz stan transformacji
+    app.rotate(math.radians(i) * math.cos(math.radians(angle))) # Obróć układ współrzędnych
+    app.line(200 * math.sin(math.radians(angle)), 0, 0, 200) # Narysuj linię
+    app.popMatrix() # Przywróć stan transformacji
+    angle += 1 # Zwiększ kąt
+    app.redraw()
  
-setup() # Wywołaj funkcję setup
- 
+#setup() # Wywołaj funkcję setup
+
 while True: # Uruchamia pętlę draw
   draw()
-  
+
   
 # Poniżej znajduje się komentaż od Gemini po wygenerowaniu kodu.
 #
